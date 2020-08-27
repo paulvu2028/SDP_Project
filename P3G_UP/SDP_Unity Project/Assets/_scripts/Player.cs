c42213a90443305d5084055a3cf28fb0e39f7bf8
paul <paulvu2028@gmail.com>
2020-08-27 20:59:44 +1200

    movement controls added to game

--- P3G_UP/SDP_Unity Project/Assets/_scripts/Player.cs
+++ P3G_UP/SDP_Unity Project/Assets/_scripts/Player.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpPower = 10f;
+    [SerializeField] float turningSpeed = 240f;
+    float gravity = 9.81f;
+    private Vector3 moveDirectionVector = new Vector3();
+
+    //component handles
+    [SerializeField] CharacterController _characterController;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,32 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movement();
+    }
+
+    //movement function is updated to check for keyboard inputs
+    public void movement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (v < 0)
+            v = 0;
+        transform.Rotate(0, h * turningSpeed * Time.deltaTime, 0);
+        if (_characterController.isGrounded)
+        {
+            bool move = (v > 0) || (h != 0);
+            moveDirectionVector = Vector3.forward * (v);
+            moveDirectionVector = transform.TransformDirection(moveDirectionVector);
+            moveDirectionVector *= speed;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                moveDirectionVector.y = jumpPower;
+            }
+        }
+
+        moveDirectionVector.y -= gravity * Time.deltaTime;
+        _characterController.Move(moveDirectionVector * Time.deltaTime);
     }
 }
