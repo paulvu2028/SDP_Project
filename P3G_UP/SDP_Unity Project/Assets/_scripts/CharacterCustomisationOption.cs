22f2fbedb70419da81af93e84cb4a268302c4ff8
paul <paulvu2028@gmail.com>
2020-09-25 11:48:52 +1200

    Revert "Revert "Added player customisation selection to change the player model""
    This reverts commit f29d9d0287413c17daf42b70adc1c25e50f5874e.

---
+++ P3G_UP/SDP_Unity Project/Assets/_scripts/CharacterCustomisationOption.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class CharacterCustomisationOption : MonoBehaviour
+{
+    public GameObject character;
+
+    public void Submit()
+    {
+        PrefabUtility.SaveAsPrefabAsset(character, "Assets/PlayerModel.prefab");
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ P3G_UP/SDP_Unity Project/Assets/_scripts/ModelChanger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelChanger : MonoBehaviour
+{
+    public SkinnedMeshRenderer customisation;
+
+    public List<Mesh> options = new List<Mesh>();
+
+    private int currentOption = 0;
+
+    public void NextOption()
+    {
+        currentOption++;
+        if(currentOption >= options.Count)
+        {
+            currentOption = 0;
+        }
+
+        customisation.sharedMesh = options[currentOption];
+    }
+
+    public void PreviousOption()
+    {
+        currentOption--;
+        if(currentOption <= 0)
+        {
+            currentOption = options.Count - 1;
+        }
+
+        customisation.sharedMesh = options[currentOption];
+    }
+}
