199b95c4dbaa03de2513ca7fb3ce3ce530f341c1
paul <paulvu2028@gmail.com>
2020-10-27 00:48:36 +1300

    shop ui update

--- P3G_UP/SDP_Unity Project/Assets/_scripts/UIShop.cs
+++ P3G_UP/SDP_Unity Project/Assets/_scripts/UIShop.cs
@@ -6,40 +6,50 @@ using TMPro;
 
 public class UIShop : MonoBehaviour
 {
-    private Transform container;
-    private Transform shopItemTemplate;
+    public GameObject SelectionImg;
+
+
+    public int _currentSelectedItem;
+    public int _currentItemCost;
+
+    public void UpdateShopSelection(int yPosition)
+    {
+        SelectionImg.GetComponent<Image>().rectTransform.anchoredPosition = new Vector2(SelectionImg.GetComponent<Image>().rectTransform.anchoredPosition.x, yPosition);
+    }
 
     private void Awake()
     {
-        container = transform.Find("Container");
-        shopItemTemplate = container.Find("ShopItemTemplate");
-        shopItemTemplate.gameObject.SetActive(false);
+
     }
 
     private void Start()
     {
-        CreateButton(Character.GetSprite(Character.CharacterType.CharacterMaleJacket), "Male Jacket", Character.GetCost(Character.CharacterType.CharacterMaleJacket), 0);
-        CreateButton(Character.GetSprite(Character.CharacterType.CharacterBusinessManSuit), "Business Suit", Character.GetCost(Character.CharacterType.CharacterBusinessManSuit), 1);
-        CreateButton(Character.GetSprite(Character.CharacterType.CharacterBusinessManShirt), "Business Shirt", Character.GetCost(Character.CharacterType.CharacterBusinessManShirt), 2);
-        CreateButton(Character.GetSprite(Character.CharacterType.CharacterBusinessWoman), "Business Woman", Character.GetCost(Character.CharacterType.CharacterBusinessWoman), 3);
-        CreateButton(Character.GetSprite(Character.CharacterType.CharacterFemaleCoat), "Female Coat", Character.GetCost(Character.CharacterType.CharacterFemaleCoat), 4);
-        CreateButton(Character.GetSprite(Character.CharacterType.CharacterFemaleJacket), "Female Jacket", Character.GetCost(Character.CharacterType.CharacterFemaleJacket), 5);
-        CreateButton(Character.GetSprite(Character.CharacterType.CharacterFemalePolice), "Female Police", Character.GetCost(Character.CharacterType.CharacterFemalePolice), 6);
-        CreateButton(Character.GetSprite(Character.CharacterType.CharacterMaleHoodie), "Male Hoodie", Character.GetCost(Character.CharacterType.CharacterMaleHoodie), 7);
-        CreateButton(Character.GetSprite(Character.CharacterType.CharacterMalePolice), "Male Police", Character.GetCost(Character.CharacterType.CharacterMalePolice), 8);
+
     }
 
-    private void CreateButton(Sprite imageSprite, string name, int cost, int position)
+    public void SelectItem(int Item)
     {
-        Transform shopItemTransform = Instantiate(shopItemTemplate, container);
-        shopItemTransform.gameObject.SetActive(true);
-        RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
+        SelectionImg.SetActive(true);
+        switch (Item)
+        {
+            //charaters 0 - 10
+            case 0:
+                UpdateShopSelection(260);
+                _currentSelectedItem = 0;
+                _currentItemCost = 10;
+                break;
+            case 1:
+                UpdateShopSelection(218);
+                _currentSelectedItem = 1;
+                _currentItemCost = 15;
+                break;
+            case 2:
+                UpdateShopSelection(174);
+                _currentSelectedItem = 1;
+                _currentItemCost = 15;
+                break;
 
-        float shopItemHeight = 50f;
-        shopItemRectTransform.anchoredPosition = new Vector2(0, -shopItemHeight * position);
-
-        shopItemTransform.Find("NameText").GetComponent<TextMeshProUGUI>().SetText(name);
-        shopItemTransform.Find("CostText").GetComponent<TextMeshProUGUI>().SetText(cost.ToString());
-        shopItemTransform.Find("Image").GetComponent<Image>().sprite = imageSprite;
+        }
     }
+
 }
