b583e6785046bba98375b8dcdac7edfa3dfac705
Encryption32 <shenweiho47@gmail.com>
2020-10-27 03:54:06 +1300

    NPC Walking

---
+++ P3G_UP/SDP_Unity Project/Assets/_scripts/NpcDest/PoliceDest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceDest : MonoBehaviour
+{
+    public int destPoint;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "NPC")
+        {
+            if (destPoint == 2)
+            {
+                destPoint = 0;
+            }
+            if (destPoint == 1)
+            {
+                this.gameObject.transform.position = new Vector3(15, 1, 69);
+                destPoint = 2;
+            }
+            if (destPoint == 0)
+            {
+                this.gameObject.transform.position = new Vector3(15, 1, 33);
+                destPoint = 1;
+            }
+        }
+    }
+
+}
